710c43ff58c680392dcc25f04e6856a1f893d4c1
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2022-12-08 20:24:55 +0100

    use css instead of extra style element for theme

--- index.js
+++ index.js
@@ -8,40 +8,21 @@
 
 window.addEventListener("DOMContentLoaded", () => {
     const toggle = () => {
-        let lightTheme = document.getElementById("light-theme")
-        if (lightTheme) {
-            document.head.removeChild(lightTheme)
-            localStorage.removeItem("theme")
-        } else {
-            lightTheme = document.createElement("style")
-            lightTheme.id = "light-theme"
-            lightTheme.textContent = `:root {
-                --text: #333;
-                --background: #eee;
-                --subtext: #555;
-                --link: #08f;
-                --menu-background: #ddd;
-                --code-background: #fff;
-                --img-border: #fff;
-                --button-text: #fff;
-                --button-primary: #f02;
-                --button-secondary: #fff;
-            }
-            #toggle-theme, .action-buttons img, footer img[src*=svg] {
-                filter: invert(.8);
-            }
-            .third-party-downloads img {
-                filter: contrast(.5) invert() contrast(4)
-                hue-rotate(180deg) saturate(4);
-            }
-            .download-button img {filter: none;}
-            .cheatsheet img {filter: none;}`
-            document.head.appendChild(lightTheme)
-            localStorage.setItem("theme", "light")
+        let newTheme = "light"
+        if (document.documentElement.getAttribute("data-theme") === "light") {
+            newTheme = "dark"
         }
+        document.documentElement.setAttribute("data-theme", newTheme)
+        localStorage.setItem("theme", newTheme)
     }
     document.getElementById("toggle-theme").addEventListener("click", toggle)
-    if (localStorage.getItem("theme") === "light") {
-        toggle()
+    let theme = "dark"
+    if (localStorage.getItem("theme")) {
+        if (localStorage.getItem("theme") === "light") {
+            theme = "light"
+        }
+    } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+        theme = "light"
     }
+    document.documentElement.setAttribute("data-theme", theme)
 })
