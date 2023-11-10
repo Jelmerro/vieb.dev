ee44bfaafc7311f1074511dee65dbdd76ee79172
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2023-11-10 10:59:37 +0100

    update version

--- download.js
+++ download.js
@@ -6,7 +6,7 @@
 // The brand logos, screenshots and github badges are public domain via CC0 1.0,
 // see ./LICENSE.CC0-1.0 or https://creativecommons.org/publicdomain/zero/1.0/
 
-const latestRelease = "10.4.0"
+const latestRelease = "10.5.0"
 const releasesList = [
     {
         "name": "Windows",
